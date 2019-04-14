4763be98a83db2497b7f80dc8ff95473b66ab3cc
Rodrigo Lopes <rodrigolmti@gmail.com>
2019-04-13 21:36:58 -0300

    Change icon

--- app/src/main/java/com/vortex/secret/App.kt
+++ app/src/main/java/com/vortex/secret/App.kt
@@ -1,8 +1,10 @@
 package com.vortex.secret
 
 import android.app.Application
+import com.crashlytics.android.Crashlytics
 import com.vortex.secret.di.dataModule
 import com.vortex.secret.di.viewModelModule
+import io.fabric.sdk.android.Fabric
 import org.koin.android.ext.android.startKoin
 
 class App : Application() {
@@ -11,5 +13,6 @@ class App : Application() {
         super.onCreate()
 
         startKoin(this, listOf(dataModule, viewModelModule))
+        Fabric.with(this, Crashlytics())
     }
 }
